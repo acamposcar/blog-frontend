83881d4a8f114d0cf27f3d2c8233c4f1db03198b
Alejandro Campos <acamposcar@gmail.com>
2022-05-13 00:53:02 +0200

    Add syntax highlight

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -3,13 +3,15 @@ import Typography from '@mui/material/Typography'
 import { formatDistanceToNow } from 'date-fns'
 import { Stack, Box } from '@mui/material'
 import Divider from '@mui/material/Divider'
+import ReactMarkdown from 'react-markdown'
+import CodeBlock from './CodeBlock'
+import remarkGfm from 'remark-gfm'
 
 const PostDetail = (props) => {
   const { post } = props
-
   return (
     <>
-      <Typography gutterBottom variant="h1" component="div" sx={{ marginTop: 4 }}>
+      <Typography gutterBottom variant="h2" component="h1" sx={{ marginTop: 4 }}>
         {post.title}
       </Typography>
       <Stack sx={{ marginY: 3 }} direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />}
@@ -32,9 +34,7 @@ const PostDetail = (props) => {
         alt="The house from the offer."
         src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
       />
-      <Typography color="text.primary" sx={{ marginY: 4 }}>
-        {post.content}
-      </Typography>
+      <ReactMarkdown components={CodeBlock} remarkPlugins={[remarkGfm]} >{post.content}</ReactMarkdown>
 
     </>
   )
