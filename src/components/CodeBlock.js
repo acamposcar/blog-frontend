223c49d05e505e0b487836143d4931d5767b9270
Alejandro Campos <acamposcar@gmail.com>
2022-05-13 13:41:08 +0200

    Change to PrismLight to reduce bundle size

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,6 +1,17 @@
 import React from 'react'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx'
+import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript'
+import json from 'react-syntax-highlighter/dist/esm/languages/prism/json'
+import bash from 'react-syntax-highlighter/dist/esm/languages/prism/bash'
+import css from 'react-syntax-highlighter/dist/esm/languages/prism/css'
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+SyntaxHighlighter.registerLanguage('jsx', jsx)
+SyntaxHighlighter.registerLanguage('javascript', js)
+SyntaxHighlighter.registerLanguage('json', json)
+SyntaxHighlighter.registerLanguage('bash', bash)
+SyntaxHighlighter.registerLanguage('css', css)
 
 const CodeBlock = {
   code({ node, inline, className, children, ...props }) {
@@ -8,7 +19,7 @@ const CodeBlock = {
     return !inline && match
       ? (
         <SyntaxHighlighter
-          style={tomorrow}
+          style={materialDark}
           language={match[1]}
           PreTag="div" {...props}>
           {String(children).replace(/\n$/, '')}
